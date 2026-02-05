5540d921809538a2f583b6173ff46c5f372dbc5f
pramod kumar <pramod7017440873@gmail.com>
2026-02-05 22:54:44 +0530

    String

---
+++ PalindromeString.js
@@ -0,0 +1,62 @@
+// let b="pramodMourya";
+
+// let start =0;
+// let end=b.length-1;
+// let isPalindrome=true;
+
+// while(start<end){
+//     if(b[start]!=b[end]){
+//         isPalindrome=false;
+//         break;
+//     }
+//     start++;
+//     end--;
+
+// }
+// if(isPalindrome){
+//     console.log("p")
+// }
+// else{
+//     console.log("NP");
+// }
+
+
+
+let c="MadaM";
+let start=0;
+let end=c.length-1;
+let isPalindrome=true;
+while(start<end){
+    if(c[start]!=c[end]){
+        isPalindrome=false;
+        break;
+    }
+    start++;
+    end--;
+
+}
+if(isPalindrome){
+    console.log("Pailindrome");
+}
+else{
+    console.log("There is not pailindrome");
+
+}
+
+
+// Second Method
+
+
+// let a = "madam";
+// let reversed = "";
+// for (let i = a.length - 1; i >= 0; i--) {
+//   reversed = reversed + a[i];
+// } 
+// if (a == reversed) {
+//   console.log("Is Palandrome");
+// } else {
+//   console.log("Not Palndrome");
+// }
+
+
+
---
+++ ReverseString.js
@@ -0,0 +1,9 @@
+//  String ko reverse karo 
+
+let a= "PramodMourya"
+let reversed="";
+for(let i=a.length-1;i>=0;i--){
+    reversed=reversed+a[i];
+}
+console.log(reversed);
+     
